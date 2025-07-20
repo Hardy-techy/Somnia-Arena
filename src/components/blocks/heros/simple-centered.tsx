c974c9376241421340d5e6c28c7b7049d89aa84f
Hardy-techy <hardyblake354@gmail.com>
2025-07-20 19:05:50 +0530

    Fix duplicate style attribute in simple-centered.tsx

--- src/components/blocks/heros/simple-centered.tsx
+++ src/components/blocks/heros/simple-centered.tsx
@@ -187,8 +187,9 @@ export default function SimpleCentered() {
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              animationDelay: '3s',
             }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#8b5cf6]/30 via-[#00ff88]/30 to-[#00d4ff]/30 opacity-80 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] animate-pulse" style={{ animationDelay: '3s' }}
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#8b5cf6]/30 via-[#00ff88]/30 to-[#00d4ff]/30 opacity-80 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] animate-pulse"
           />
         </div>
       </div>
