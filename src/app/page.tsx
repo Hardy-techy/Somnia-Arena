86eeac660a9f51acd77476161d0ffc1da95e93fe
Hardy-techy <hardyblake354@gmail.com>
2025-07-20 18:57:25 +0530

    Remove unused footer import and usage to fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import SimpleCentered from '@/components/blocks/heros/simple-centered'
 import GamesShowcase from '@/components/blocks/games/cosmic-games-showcase'
-import { SimpleFooterWithFourGrids } from '@/components/blocks/footers/simple-footer-with-four-grids'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#0f0f0f]">
       <SimpleCentered />
       <GamesShowcase />
-      {/* <SimpleFooterWithFourGrids /> */}
     </div>
   )
 }
